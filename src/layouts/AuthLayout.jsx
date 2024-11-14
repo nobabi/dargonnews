04b6f44f9a7d158540133b2f67cbf5176933d31a
nobabi <sherajul529@gmail.com>
2024-11-14 18:52:30 +0600

    added loing page

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const AuthLayout = () => {
     return (
-        <div>
+        <div className="font-poppins bg-[#f3f3f3]">
+            <header className="py-3 w-11/12 mx-auto">
+            <Navbar></Navbar>
+            </header>
             <Outlet></Outlet>
         </div>
     );
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -8,6 +8,8 @@ import {
 import HomeLayout from '../layouts/HomeLayout';
 import CategoryNews from '../pages/CategoryNews';
 import AuthLayout from '../layouts/AuthLayout';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 const router = createBrowserRouter([
   {
@@ -44,11 +46,11 @@ const router = createBrowserRouter([
     children: [
      {
       path: "/auth/login",
-      element: <h2>Login</h2>,
+      element: <Login></Login>,
      },
      {
       path: "/auth/register",
-      element: <h2>Register</h2>,
+      element: <Register></Register>
      },
     ]
   },
